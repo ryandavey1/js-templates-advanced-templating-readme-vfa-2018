fc7fab3d712151209b3dea6d9b641ca721219c40
Ryan Davey <ryancdavey@icloud.com>
2018-07-02 06:08:58 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,18 @@
-const expect = require('expect')
+// const expect = require('expect')
 
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-
-
-describe('index', () => {
-
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
+// const fs = require('fs')
+// const jsdom = require('mocha-jsdom')
+// const path = require('path')
 
 
-  it('runs', () => {
-    expect(true).toBe.true
-  })
-})
+// describe('index', () => {
+
+//   jsdom({
+//     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+//   })
+
+
+//   it('runs', () => {
+//     expect(true).toBe.true
+//   })
+// })
